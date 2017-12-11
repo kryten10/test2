c8b056dee79587037b6e08d1e937b0234ed44407
kryten10 <rupert@tmw.org>
2017-12-11 20:32:47 +0000

    more

--- test_debugger.py
+++ test_debugger.py
@@ -7,6 +7,9 @@ class TestSortList(unittest.TestCase):
     def test_sort_list(self):
         x = [0,1,2,3,4,5,6,7,8]
         random.shuffle(x)
-        self.assertEqual()
+        self.assertEqual([0,1,2,3,4,5,6,7,8], sort_list(x))
+
+        if __name__ == __main__:
+
 
 
