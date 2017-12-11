c667d9261a1b5b1756b3a93d62f9b802b216e660
kryten10 <rupert@tmw.org>
2017-12-11 20:27:19 +0000

    more

---
+++ test_debugger.py
@@ -0,0 +1,12 @@
+import test_debugger
+
+from debugger import sort_list
+
+class TestSortList(unittest.TestCase):
+
+    def test_sort_list(self):
+        x = [0,1,2,3,4,5,6,7,8]
+        random.shuffle(x)
+        self.assertEqual()
+
+
